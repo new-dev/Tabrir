672c1a8be7da7485aa3b24fbf9bb6dee33fe3d20
t_tappa <alex.tappin@autodesk.com>
2016-02-04 15:33:36 -0800

    Angular2 app working locally (not in the plugin since I need to dl the files locallly)

--- popup.js
+++ popup.js
@@ -1,11 +1,34 @@
-import {bootstrap} from "angular2/platform/browser";
-import {Component} from "angular2/core";
+(function() {
 
-@Component({
-    selector: 'app',
-    template: '<div>Hi</div>'
-});
+    var HelloWorldComponent = function() {};
 
-class App{}
+    HelloWorldComponent.annotations = [
+        new ng.core.Component({
+            selector: 'hello-world',
+            template: '<h1>Hello Angular2!</h1>'
+        })
+    ];
 
-bootstrap(App);
+    var HelloFlentApi = ng.core.Component({
+        selector: 'hello-fluent',
+        template: '<h1>Hello {{name}}!</h1>' + '<input [(ngModel)]="name">',
+    }).Class({
+        constructor: function() {
+            this.name = "Fluent API";
+        }
+    });
+
+    var AppComponent = ng.core.Component({
+        selector: 'company-app',
+        template: '<hello-world></hello-world>' +
+        '<hello-fluent></hello-fluent>',
+        directives: [HelloWorldComponent, HelloFlentApi]
+    }).Class({
+        constructor: function() {}
+    });
+
+    document.addEventListener("DOMContentLoaded", function() {
+        ng.platform.browser.bootstrap(AppComponent);
+    });
+
+}());
