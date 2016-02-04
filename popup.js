d308688e5f327b648a4f94de19bdec5f1db4f917
t_tappa <alex.tappin@autodesk.com>
2016-02-04 15:58:33 -0800

    Angular2 works now. It says hello

--- popup.js
+++ popup.js
@@ -1,34 +1,13 @@
 (function() {
-
-    var HelloWorldComponent = function() {};
-
-    HelloWorldComponent.annotations = [
-        new ng.core.Component({
-            selector: 'hello-world',
-            template: '<h1>Hello Angular2!</h1>'
-        })
-    ];
-
-    var HelloFlentApi = ng.core.Component({
-        selector: 'hello-fluent',
-        template: '<h1>Hello {{name}}!</h1>' + '<input [(ngModel)]="name">',
-    }).Class({
-        constructor: function() {
-            this.name = "Fluent API";
-        }
-    });
-
-    var AppComponent = ng.core.Component({
-        selector: 'company-app',
-        template: '<hello-world></hello-world>' +
-        '<hello-fluent></hello-fluent>',
-        directives: [HelloWorldComponent, HelloFlentApi]
+    var blahComponent = ng.core.Component({
+        selector: 'blah-app',
+        template: '<div>hello</div>'
     }).Class({
         constructor: function() {}
     });
 
     document.addEventListener("DOMContentLoaded", function() {
-        ng.platform.browser.bootstrap(AppComponent);
+        ng.platform.browser.bootstrap(blahComponent);
     });
 
 }());
