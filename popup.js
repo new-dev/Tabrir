b1caa7fced11daca197ec530e1cbc70c98a7f3f3
t_tappa <alex.tappin@autodesk.com>
2016-02-04 17:17:13 -0800

    self invoking function description

--- popup.js
+++ popup.js
@@ -1,3 +1,5 @@
+//This is a self invoking function. if you notice at the end of the "function" there is "()"
+
 (function() {
     var blahComponent = ng.core.Component({
         selector: 'blah-app',
