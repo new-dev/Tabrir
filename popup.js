e7ea3e9fe0fac19d5806ae7cd7bf8f04e064ffd1
= <timothyjamesjensen@gmail.com>
2016-04-05 18:29:51 -0700

    sample tab opening

--- popup.js
+++ popup.js
@@ -3,4 +3,10 @@ var app = angular.module('app', []);
 
 app.controller('appCtrl', function($scope){
 
+    var urlList = [];
+
+    urlList.push("https://google.com");
+    urlList.push("https://angular.io/");
+
+    urlList.map(url => chrome.tabs.create({url: url}));
 });
