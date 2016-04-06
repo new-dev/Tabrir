b9aa65ca95fe6db54dd2ee53569bac2a6687c56d
= <timothyjamesjensen@gmail.com>
2016-04-05 19:07:41 -0700

    commented out testing code in controller

--- popup.js
+++ popup.js
@@ -3,7 +3,8 @@ var app = angular.module('app', []);
 
 app.controller('appCtrl', function($scope, tabService){
 
-    var urlList = ["https://google.com", "https://angular.io/"];
-    tabService.openUrls(urlList);
+    //Uncomment the below two lines to test open url's
+    //var urlList = ["https://google.com", "https://angular.io/"];
+    //tabService.openUrls(urlList);
 
 });
