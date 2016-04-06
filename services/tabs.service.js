0b25c96e15174773865a095cd076c888a9d50d72
= <timothyjamesjensen@gmail.com>
2016-04-05 18:37:55 -0700

    created sample method in tabs.service

--- services/tabs.service.js
+++ services/tabs.service.js
@@ -1,4 +1,13 @@
 angular.module('app')
     .service('tabs.service', [
+        function() {
+            this.open = function() {
+                var urlList = [];
 
+                urlList.push("https://google.com");
+                urlList.push("https://angular.io/");
+
+                urlList.map(url => chrome.tabs.create({url: url}));
+            }
+        }
     ]);
