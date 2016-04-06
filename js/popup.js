7c3668bd1b1a4d51b27f04845065caa71fe1ce96
Timothy Jensen <timothyjamesjensen@gmail.com>
2016-04-06 15:00:43 -0700

    removed comments from controller

--- js/popup.js
+++ js/popup.js
@@ -3,10 +3,6 @@ var app = angular.module('app', []);
 
 app.controller('appCtrl', function($scope, groupFactory){
 
-    //Uncomment the below two lines to test open url's
-    //var urlList = ["https://google.com", "https://angular.io/"];
-    //tabService.openUrls(urlList);
-
     $scope.createGroup = function() {
         groupFactory.newGroup();
     };
