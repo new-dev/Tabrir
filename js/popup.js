09575e676913f861c85d5648a2ed7a7b0fb7330e
= <timothyjamesjensen@gmail.com>
2016-04-05 20:09:19 -0700

    injected groupFactory into controller. create function to call its create

--- js/popup.js
+++ js/popup.js
@@ -1,10 +1,13 @@
 
 var app = angular.module('app', []);
 
-app.controller('appCtrl', function($scope, tabService){
+app.controller('appCtrl', function($scope, tabService, groupFactory){
 
     //Uncomment the below two lines to test open url's
     //var urlList = ["https://google.com", "https://angular.io/"];
     //tabService.openUrls(urlList);
 
+    $scope.createGroup = function() {
+        groupFactory.newGroup();
+    }
 });
