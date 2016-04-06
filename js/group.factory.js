e8546435fcd45ad89587eb44ce4fa4bb0a25afd5
= <timothyjamesjensen@gmail.com>
2016-04-05 19:53:13 -0700

    created method shells for basic groupFactory functionality

--- js/group.factory.js
+++ js/group.factory.js
@@ -1,5 +1,21 @@
 angular.module('app').factory('groupFactory', function() {
-    var groupFactory = {}
+    var groupFactory = {};
+
+    groupFactory.newGroup = function() {
+
+    };
+
+    groupFactory.saveGroup = function() {
+
+    };
+
+    groupFactory.editGroup = function() {
+
+    };
+
+    groupFactory.deleteGroup = function() {
+
+    };
 
     return groupFactory;
 });
