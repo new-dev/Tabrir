5f080cf9cedf45675da3039b1ba29aa360a33281
alextappin <alextappin@gmail.com>
2016-04-06 20:22:32 -0700

    Can dynamically add and delete url from a tab group

--- js/newGroup.js
+++ js/newGroup.js
@@ -2,11 +2,18 @@
  * Created by ajt on 4/6/2016.
  */
 angular.module('app').controller('newGroupCtrl', function($scope, groupFactory, iconService) {
+    $scope.tempUrls = ["www.google.com"];
+
+    $scope.addUrlBox = function addUrlBox() {
+        $scope.tempUrls.push("");
+    };
+    $scope.removeUrl = function removeUrl(index) {
+        $scope.tempUrls.splice(index, 1);
+    };
 
     $scope.createGroup = function() {
         groupFactory.create();
     };
-
     $scope.getIcon = function(url) {
         $scope.icon = iconService.favicon(url);
     }
