8cecacd522c27ef6278cab20a3f7f2fe7db960ea
Timothy Jensen <timothyjamesjensen@gmail.com>
2016-04-06 17:20:58 -0700

    refactored iconService for arrow functions

--- js/services/icon.service.js
+++ js/services/icon.service.js
@@ -1,9 +1,6 @@
 angular.module('app').service('iconService', function() {
 
-   this.getFavicon = (url) => {
-       var api = "http://www.google.com/s2/favicons?domain= ";
-       api += url;
-       return api;
-   }
+   var faviconAPI = "http://www.google.com/s2/favicons?domain=";
+   this.getFavicon = (url) => faviconAPI += url;
 
 });
