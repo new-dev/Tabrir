0b1efd27bc15382d6ba1bb9a270730949880dca6
Timothy Jensen <timothyjamesjensen@gmail.com>
2016-04-06 17:48:16 -0700

    created chromeStorageService

---
+++ js/services/chrome.storage.service.js
@@ -0,0 +1,27 @@
+angular.module('app').service('chromeStorageService', function() {
+
+    this.save = (newGroupData) => {
+        chrome.storage.sync.get(storedGroups => {
+            if(typeof(storedGroups.data) !== 'undefined' && storedGroups.data instanceof Array) {
+                storedGroups.data.unshift(newGroupData);
+            } else {
+                storedGroups.data = [newGroupData];
+            }
+            chrome.storage.sync.set(storedGroups, () => {
+                if (chrome.runtime.error) {
+                    console.log("RuntimeError.");
+                }
+            });
+            window.close();
+        });
+    }
+
+    this.load = (deferred) => {
+        chrome.storage.sync.get(storedGroups => {
+            if (!chrome.runtime.error) {
+                deferred.resolve(storedGroups.data);
+            }
+        });
+    }
+
+});
