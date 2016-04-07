614a9af7455719b97062a8e4d803fe470561ee78
alextappin <alextappin@gmail.com>
2016-04-06 19:55:52 -0700

    View manager is added and working.

---
+++ js/viewManager.js
@@ -0,0 +1,23 @@
+/**
+ * Created by ajt on 4/6/2016.
+ */
+angular.module('app').controller('viewManagerCtrl', function($scope) {
+    var currentView = "default";
+    $scope.switchToCreateView = function switchToCreateView() {
+        currentView = "create";
+    };
+    $scope.switchToDefaultView = function switchToDefaultView() {
+        currentView = "default";
+    };
+    $scope.showDefaultView = function showDefaultView() {
+        return currentView == "default";
+    };
+    $scope.showCreateView = function showCreateView() {
+        return currentView == "create";
+    }
+});
+
+//eventually we can break each part into its own directive as well
+angular.module('app').directive('manageview', function() {
+    return { scope: false, templateUrl: '../../views/templates/manage-views.html' }
+});
