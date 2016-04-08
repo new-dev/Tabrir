51364b677c55c1140fac6e4802d3c7d56104a6c5
Timothy Jensen <timothyjamesjensen@gmail.com>
2016-04-08 11:09:24 -0700

    removed document.getelementbyid. switched to data binding

--- js/newGroup.js
+++ js/newGroup.js
@@ -2,10 +2,11 @@
 
 angular.module('app').controller('newGroupCtrl', function($scope, groupFactory, iconService) {
     $scope.tempUrls = ["www.google.com"];
+    $scope.groupName;
 
     $scope.addUrlBox = () => $scope.tempUrls.push("");
     $scope.removeUrl = (index) => $scope.tempUrls.splice(index, 1);
-    $scope.createGroup = () => groupFactory.create(document.getElementById("group-name").value, $scope.tempUrls);
+    $scope.createGroup = () => groupFactory.create($scope.groupName, $scope.tempUrls);
     $scope.getIcon = (url) => iconService.favicon(url);
 });
 
