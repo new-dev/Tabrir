23f803afa34965e84632199843a8a67ab617f71e
Timothy Jensen <timothyjamesjensen@gmail.com>
2016-04-08 19:25:25 -0700

    Runs getData on startup of the view so that it can be displayed

--- js/viewManager.js
+++ js/viewManager.js
@@ -7,13 +7,10 @@ angular.module('app').controller('viewManagerCtrl', function($scope, groupFactor
     $scope.switchToDefaultView = () => currentView = "default";
     $scope.showDefaultView = () => currentView === "default";
     $scope.showCreateView = () => currentView === "create";
-    var init = () => groupFactory.groups();
-
-    init();
 });
 
 //eventually we can break each part into its own directive as well
 angular.module('app').directive('manageview', function(groupFactory) {
-    //groupFactory.groups();
+    groupFactory.groups();
     return { scope: false, templateUrl: '../../views/templates/manage-views.html' }
 });
